d55d80e16fbc9bcfde09feb3deeba81882635d48
Grish Khechoyan <grishkhechoyan1@gmail.com>
2025-04-13 21:37:38 +0400

    edit txt

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editttttt <code>src/App.js</code> and save to reload.
+          Grish <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
